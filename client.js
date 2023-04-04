23141fa534833a82db1ff3fe745095b61e02ba70
Yash sharma <71271069+Yashsharma1911@users.noreply.github.com>
2023-04-04 08:29:59 +0000

    Implemented microsoft provider to access user data
    Signed-off-by: Yash sharma <71271069+Yashsharma1911@users.noreply.github.com>

--- client.js
+++ client.js
@@ -1,10 +1,5 @@
+import {Providers, Msal2Provider } from "@microsoft/mgt";
 
-const options = {
-    // TODO: add more options here manav and anushka
-	authProvider,
-};
-
-const client = Client.init(options);
-
-let messages = await client.api('/me/messages')
-	.get();
+Providers.globalProvider = new Msal2Provider({
+  clientId: 'fb9db9e2-939d-401c-88a4-e90909eefc42'
+});
